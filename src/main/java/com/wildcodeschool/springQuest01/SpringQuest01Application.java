17f0b97711caf743ea687fdacbd8a873bb60b54c
Boumedienne-dev <bmessadia83@gmail.com>
2022-11-14 13:43:43 +0100

    quete 01 spring-route

---
+++ src/main/java/com/wildcodeschool/springQuest01/SpringQuest01Application.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.springQuest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuest01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuest01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/springQuest01/SpringQuest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.springQuest01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuest01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
